2527f50c6e426d7da20e86a3d7dcbefa44af639c
Viktoriia <lyudkevych.v@gmail.com>
2025-07-29 18:38:54 +0600

    Refactoring

--- app/main.py
+++ app/main.py
@@ -1,16 +1,12 @@
 from fastapi import FastAPI
-from fastapi import FastAPI
 from app.routers import profiles
 from app.database import Base, engine
 from app import models
 
-# Создание таблиц при запуске
 Base.metadata.create_all(bind=engine)
 
-# Инициализация FastAPI
 app = FastAPI()
 
-# Роуты
 app.include_router(profiles.router, prefix="/profiles", tags=["Profiles"])
 
 @app.get("/")
