92137c494e8eca791f774e5fa8389e19e1840b60
Viktoriia <lyudkevych.v@gmail.com>
2025-07-29 19:29:13 +0600

    fix: add missing CORS import

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import profiles
 from app.database import Base, engine
 from app import models
@@ -6,6 +7,14 @@ from app import models
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://inclusive-matching-frontend2.vercel.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(profiles.router, prefix="/profiles", tags=["Profiles"])
 
