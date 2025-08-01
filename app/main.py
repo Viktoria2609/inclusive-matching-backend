2606e1eac51477ecc78badadfc5540e74dba5d21
Viktoriia <lyudkevych.v@gmail.com>
2025-08-01 21:15:28 +0600

    Cors uppdate

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://inclusive-matching-frontend2.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
